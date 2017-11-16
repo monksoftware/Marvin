542d3b863fab40a3ac51f7852dee6f827cbc134c
Alessio Arsuffi <arsuffi@monksoftware.it>
2017-11-16 18:38:35 +0100

    bump to version 0.1 for MarvinUI

--- MarvinUI/MarvinUI.h
+++ MarvinUI/MarvinUI.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double MarvinUIVersionNumber;
 FOUNDATION_EXPORT const unsigned char MarvinUIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MarvinUI/PublicHeader.h>
-
-
